5d5bb9b9698450898a23d6ab1f3ef8e4f61e1670
hyomin <miamiq0000@gmail.com>
2025-08-05 00:33:30 +0900

    x

--- src/components/game-over/game-over.tsx
+++ src/components/game-over/game-over.tsx
@@ -4,9 +4,8 @@ export default function GameOver() {
   return (
     <>
       <div className="relative bg-background rounded-xl w-full h-full flex items-center justify-center flex-col">
-        <div className="text-3xl mb-12">🎉 클리어!</div>
-
-        <div className="select-none touch-none flex flex-col justify-center items-center max-w-screen-sm w-full p-2">
+        <div className="select-none touch-none flex flex-col justify-center items-center max-w-screen-sm w-full p-4 py-12">
+          <div className="text-3xl mb-6">🎉 클리어!</div>
           <AppleBox />
         </div>
       </div>
